f80c807fa20160f6b0a7be306764c5746f551505
boulevard-h <1549819758@qq.com>
2025-03-27 19:21:52 +0800

    Add duration calculate for NS/NL/RC

---
+++ cmd/duration/durationCal.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// 计算平均Duration
+func CalculateAverageDuration(dir string) (float64, error) {
+	durationReg := regexp.MustCompile(`Duration:\s*([\d\.]+)ms`)
+
+	var totalDuration float64
+	var durationCount int
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if strings.HasPrefix(info.Name(), "(Performance)") {
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				line := scanner.Text()
+
+				if matches := durationReg.FindStringSubmatch(line); matches != nil {
+					duration, err := strconv.ParseFloat(matches[1], 64)
+					if err == nil {
+						totalDuration += duration
+						durationCount++
+					}
+				}
+			}
+
+			if err := scanner.Err(); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	if durationCount == 0 {
+		return 0, fmt.Errorf("no duration data found")
+	}
+
+	return totalDuration / float64(durationCount), nil
+}
+
+func main() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error getting home directory:", err)
+		return
+	}
+
+	avgDuration, err := CalculateAverageDuration(homeDir + "/Chamael/log/")
+	if err != nil {
+		fmt.Println("Error calculating average duration:", err)
+	} else {
+		fmt.Printf("Average Duration: %.2f ms\n", avgDuration)
+	}
+}
