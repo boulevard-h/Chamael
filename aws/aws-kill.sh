02207f70a0fd192689889e83a7ca63d0e18d55c0
Yuxuan Hu <yuxuanhu@buaa.edu.cn>
2026-03-17 11:19:17 +0000

    feat: add aws-kill.sh & aws-pull.sh

---
+++ aws/aws-kill.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# the number of AWS servers to remove
+N=xxx
+
+# the number of nodes on each server
+node=xxx
+
+# public IPs --- This is the public IPs of AWS servers
+pubIPsVar=(
+[0]='xxx'
+[1]='xxx'
+[2]='xxx'
+)
+
+# kill 所有远程机器上名为 main 的进程
+i=0
+while [ $i -le $(( N-1 )) ]; do
+    ssh -o "StrictHostKeyChecking no" -i "/home/ubuntu/Chamael.pem" ubuntu@${pubIPsVar[i]} \
+    "pkill -f './main' 2>/dev/null; echo 'killed main on ${pubIPsVar[i]}'" &
+    i=$(( i+1 ))
+done
+
+wait
+echo "所有服务器上的 main 进程已终止"
---
+++ aws/aws-pull.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# the number of AWS servers to remove
+N=xxx
+
+# the number of nodes on each server
+node=xxx
+
+# public IPs --- This is the public IPs of AWS servers
+pubIPsVar=(
+[0]='xxx'
+[1]='xxx'
+[2]='xxx'
+)
+
+# 远程更新代码并预热 Go build cache
+i=0
+while [ $i -le $(( N-1 )) ]; do
+    (
+    echo "[➤] 开始处理服务器 ${pubIPsVar[i]}"
+
+    ssh -o "StrictHostKeyChecking no" -i "/home/ubuntu/Chamael.pem" ubuntu@${pubIPsVar[i]} '
+        set -e
+        cd /home/ubuntu/Chamael
+
+        echo "[1/3] git pull"
+        git pull --ff-only
+
+        echo "[2/3] warm up cmd/main/main.go build cache"
+        go run ./cmd/main/main.go /tmp/chamael-cache-warmup-missing.yaml 0 "2099-01-01 00:00:00.000" >/tmp/chamael-main-warmup.log 2>&1 || true
+
+        echo "[3/3] warm up cmd/txsMaker/txsMaker.go build cache"
+        go run ./cmd/txsMaker/txsMaker.go >/tmp/chamael-txsmaker-warmup.log 2>&1 || true
+    '
+
+    status=$?
+    if [ $status -eq 0 ]; then
+        echo "[✓] 完成服务器 ${pubIPsVar[i]}"
+    else
+        echo "[✗] 失败服务器 ${pubIPsVar[i]} (exit=${status})"
+    fi
+    ) &
+    i=$(( i+1 ))
+done
+
+wait
+echo "所有服务器处理完成"
